95ce0309f23f8bb41bd09046b54cf776ee74baa9
Rafae1-iT <rafae1nasyrov@mail.ru>
2022-04-09 10:04:36 +1000

    Notes added

---
+++ HW2/HW2/EditorPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HW2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPage : ContentPage
+    {
+        public string text;
+        public EditorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            text = e.NewTextValue;
+        }
+    }
+}
--- HW2/HW2/MainPage.xaml.cs
+++ HW2/HW2/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,27 @@ namespace HW2
 {
     public partial class MainPage : ContentPage
     {
+        ObservableCollection<string> list = new ObservableCollection<string>();
         public MainPage()
         {
             InitializeComponent();
+
+            list.Add("Заметка 4");
+            list.Add("Заметка 5");
+            list.Add("Заметка 6");
+            BindableLayout.SetItemsSource(notes, list);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var editor = new EditorPage();
+            editor.Disappearing += (sender1, e1) =>
+            {
+                list.Add(editor.text);
+            };
+            Navigation.PushAsync(editor);
         }
     }
+
+    
 }
